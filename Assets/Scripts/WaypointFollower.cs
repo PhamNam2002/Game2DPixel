600ef90ac04b810f5d22dcefbc6bf60d3fec444e
Tên của bạn <email@ca bn>
2023-11-14 18:48:23 +0700

    init project

---
+++ Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject[] waypoints;
+    private int currnetWaypointIndex = 0;
+    [SerializeField] private float speed = 2f;
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currnetWaypointIndex].transform.position,transform.position)< .1f)
+        {
+            currnetWaypointIndex++;
+            if (currnetWaypointIndex >=waypoints.Length)
+            {
+                currnetWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position,waypoints[currnetWaypointIndex].transform.position,Time.deltaTime*speed);
+    }
+}
